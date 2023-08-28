7301d2b4be4bcae94daeb9971926e87a8fff1d61
Reolight <95131769+Reolight@users.noreply.github.com>
2023-08-28 19:03:36 +0300

    Two extension methods created for sorting service

---
+++ SorterByCriteria/FilterSorterPaginatorServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SorterByCriteria;
+
+public static class FilterSorterPaginatorServiceExtensions
+{
+    public static (IEnumerable<TObject> ResultData, int MaxPages) ApplyFilterSortingPaginator<TObject>(
+        this IQueryable<TObject> queryable, IFilterSorterPaginatorService fspService, string query) =>
+        fspService.GetFilteredSortedPaginatedData(queryable, query);
+
+    public static IQueryable<TObject> GetFilteredSortedData<TObject>(this IQueryable<TObject> queryable,
+        IFilterSorterPaginatorService fspService, string jsonQuery)
+        => fspService.GetFilteredSortedData(queryable, jsonQuery);
+}
